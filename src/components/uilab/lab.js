842aa73645ec0b592301e9648bea8f530d4528b4
Vidit <vidit.tamrakar16@gmail.com>
2023-11-28 12:42:11 +0530

    files added

--- src/components/uilab/lab.js
+++ src/components/uilab/lab.js
@@ -10,6 +10,8 @@ function Lab(){
 
     const lab4 = "/UIUX/lab4/221b440_lab4_Pierce.pdf"
     const lab7 = "https://lab7juetpage.netlify.app/"
+    const lab1 = "/UIUX/lab1uiux.pdf"
+    const lab9 = "/UIUX/CR7.mp4"
     const openLabFolder = () => {
         window.open(lab4, '_blank');
       };
@@ -42,6 +44,14 @@ function Lab(){
 
       };
 
+      const openLab1Folder = () =>{
+        window.open(lab1, '_blank');
+      }
+
+      const openLab9Folder = () =>{
+        window.open(lab9, '_blank');
+      }
+
     const openLab8Folder = () => {
         showfolder(false)
         show8(true)
@@ -56,6 +66,12 @@ function Lab(){
        <div className="bab">
              <div  className={folder?"lab":"gayab"} >
 
+                <div className="item" onClick={openLab1Folder}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="gray" class="bi bi-folder2-open" viewBox="0 0 16 16">
+                <path d="M1 3.5A1.5 1.5 0 0 1 2.5 2h2.764c.958 0 1.76.56 2.311 1.184C7.985 3.648 8.48 4 9 4h4.5A1.5 1.5 0 0 1 15 5.5v.64c.57.265.94.876.856 1.546l-.64 5.124A2.5 2.5 0 0 1 12.733 15H3.266a2.5 2.5 0 0 1-2.481-2.19l-.64-5.124A1.5 1.5 0 0 1 1 6.14zM2 6h12v-.5a.5.5 0 0 0-.5-.5H9c-.964 0-1.71-.629-2.174-1.154C6.374 3.334 5.82 3 5.264 3H2.5a.5.5 0 0 0-.5.5zm-.367 1a.5.5 0 0 0-.496.562l.64 5.124A1.5 1.5 0 0 0 3.266 14h9.468a1.5 1.5 0 0 0 1.489-1.314l.64-5.124A.5.5 0 0 0 14.367 7H1.633z"/>
+                </svg>
+                <h4>Lab 1</h4>
+                </div>
                 <div className="item" onClick={openLab2Folder}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="gray" class="bi bi-folder2-open" viewBox="0 0 16 16">
                 <path d="M1 3.5A1.5 1.5 0 0 1 2.5 2h2.764c.958 0 1.76.56 2.311 1.184C7.985 3.648 8.48 4 9 4h4.5A1.5 1.5 0 0 1 15 5.5v.64c.57.265.94.876.856 1.546l-.64 5.124A2.5 2.5 0 0 1 12.733 15H3.266a2.5 2.5 0 0 1-2.481-2.19l-.64-5.124A1.5 1.5 0 0 1 1 6.14zM2 6h12v-.5a.5.5 0 0 0-.5-.5H9c-.964 0-1.71-.629-2.174-1.154C6.374 3.334 5.82 3 5.264 3H2.5a.5.5 0 0 0-.5.5zm-.367 1a.5.5 0 0 0-.496.562l.64 5.124A1.5 1.5 0 0 0 3.266 14h9.468a1.5 1.5 0 0 0 1.489-1.314l.64-5.124A.5.5 0 0 0 14.367 7H1.633z"/>
@@ -97,36 +113,45 @@ function Lab(){
                 </svg>
                 <h4>Lab 8</h4>
                 </div>
+
+                <div className="item" onClick={openLab9Folder}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="gray" class="bi bi-folder2-open" viewBox="0 0 16 16">
+                <path d="M1 3.5A1.5 1.5 0 0 1 2.5 2h2.764c.958 0 1.76.56 2.311 1.184C7.985 3.648 8.48 4 9 4h4.5A1.5 1.5 0 0 1 15 5.5v.64c.57.265.94.876.856 1.546l-.64 5.124A2.5 2.5 0 0 1 12.733 15H3.266a2.5 2.5 0 0 1-2.481-2.19l-.64-5.124A1.5 1.5 0 0 1 1 6.14zM2 6h12v-.5a.5.5 0 0 0-.5-.5H9c-.964 0-1.71-.629-2.174-1.154C6.374 3.334 5.82 3 5.264 3H2.5a.5.5 0 0 0-.5.5zm-.367 1a.5.5 0 0 0-.496.562l.64 5.124A1.5 1.5 0 0 0 3.266 14h9.468a1.5 1.5 0 0 0 1.489-1.314l.64-5.124A.5.5 0 0 0 14.367 7H1.633z"/>
+                </svg>
+                <h4>Lab 9</h4>
+                </div>
         </div>
 
         <div className={ulab2?"lab2":"gayab"}>
-                    <a href="/UIUX/lab2/221b440_lab2_ Concatenation.jpg">img1</a>
-                    <a href="/UIUX/lab2/221b440_lab2_back_color_edit.jpg">img2</a>
-                    <a href="/UIUX/lab2/221b440_lab2_clone.jpg">img3</a>
-                    <a href="/UIUX/lab2/221b440_lab2_color_manipulation.jpg">img4</a>
-                    <a href="/UIUX/lab2/221b440_lab2_eraser.jpg">img5</a>
-                    <a href="/UIUX/lab2/221b440_lab2_image_crop.jpg">img6</a>
-                    <a href="/UIUX/lab2/221b440_lab2_imager_resize.jpg">img7</a>
-                    <a href="/UIUX/lab2/221b440_lab2_operation5(moving image).jpg">img8</a>
-                    <a href="/UIUX/lab2/221b440_lab2_save_in_jpg.jpg">img9</a>
-                    <a href="/UIUX/lab2/221b440_lab2_text.jpg">img10</a>
+                    <a href="/UIUX/lab2/221b440_lab2_ Concatenation.jpg" target='blank'>Concatination</a>
+                    <a href="/UIUX/lab2/221b440_lab2_back_color_edit.jpg"  target='blank'>Background Color edit</a>
+                    <a href="/UIUX/lab2/221b440_lab2_clone.jpg"  target='blank'>Clone Image</a>
+                    <a href="/UIUX/lab2/221b440_lab2_color_manipulation.jpg" target='blank'>Color Manipulation</a>
+                    <a href="/UIUX/lab2/221b440_lab2_eraser.jpg" target='blank'>Eraser</a>
+                    <a href="/UIUX/lab2/221b440_lab2_image_crop.jpg" target='blank'>Image Crop</a>
+                    <a href="/UIUX/lab2/221b440_lab2_imager_resize.jpg" target='blank'>Image Resize</a>
+                    <a href="/UIUX/lab2/221b440_lab2_operation5(moving image).jpg" target='blank'>Moving Image</a>
+                    <a href="/UIUX/lab2/221b440_lab2_save_in_jpg.jpg" target='blank'>Image JPG Format</a>
+                    <a href="/UIUX/lab2/221b440_lab2_text.jpg" target='blank'>Text Image</a>
                 </div>
 
                 <div className={ulab3?"lab3":"gayab"} >
-                    <a href="/UIUX/lab3/221b440_lab3_ magnify.jpg">img1</a>
-                    <a href="/UIUX/lab3/221b440_lab3_hair_color.jpg">img2</a>
+                    <a href="/UIUX/lab3/221b440_lab3_ magnify.jpg" target='blank'>Magnify Effect</a>
+                    <a href="/UIUX/lab3/221b440_lab3_hair_color.jpg" target='blank'>Change Hair Color</a>
                 </div>
 
                 <div className={ulab5?"lab5":"gayab"}>
                   
-                    <a href="/UIUX/lab5/221b440_lab5_pencil.pdf">pdf1</a>
-                    <a href="/UIUX/lab5/221b440_lab5_play_button.pdf">pdf2</a>
+                    <a href="/UIUX/lab5/221b440_lab5_pencil.pdf" target='blank'>Pencil</a>
+                    <a href="/UIUX/lab5/221b440_lab5_play_button.pdf" target='blank'>Play Button</a>
                 </div>
 
                 <div className={ulab8?"lab8":"gayab"}>
-                    <a href="/UIUX/Lab-8/1-How to Configure IIS.pdf">pdf1</a>
-                    <a href="/UIUX/Lab-8/3-How to Uninstall IIS.pdf">pdf2</a>
+                    <a href="/UIUX/Lab-8/1-How to Configure IIS.pdf" target='blank'>Configure IIS</a>
+                    <a href="/UIUX/Lab-8/3-How to Uninstall IIS.pdf" target='blank'>Uninstall IIS</a>
                 </div>
+
+    
        </div>
     )
   }
