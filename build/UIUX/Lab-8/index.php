7852d956a55b84cf150d1fd0c4a7f8db7c4bb9f1
Vidit <vidit.tamrakar16@gmail.com>
2023-11-28 12:43:41 +0530

    build added

---
+++ build/UIUX/Lab-8/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<title>Untitled Document</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<style type="text/css">
+<!--
+.style1 {font-size: 24px}
+.style2 {color: #FF0000}
+-->
+</style>
+</head>
+<body>
+<div align="center" class="style1">
+  <p>Jaypee University of Engineering and Technology </p>
+  <p class="style2">This is my first php page   </p>
+</div>
+</body>
+</html>
