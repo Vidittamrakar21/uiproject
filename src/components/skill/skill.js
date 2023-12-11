0a8bfcc8592fb50b79ba41b8ef7bb9a806e620de
Vidit <vidit.tamrakar16@gmail.com>
2023-12-11 20:52:12 +0530

    u

--- src/components/skill/skill.js
+++ src/components/skill/skill.js
@@ -16,30 +16,12 @@ function Skill(){
         <div id='skill'>
             <h1>My Tech Stack</h1>
             <div id="icon">
-                <div className="mon" data-aos ={"zoom-in"}>
-                    <img src="/images/html.png" alt="" />
-                </div>
-                <div className="mon" data-aos ={"zoom-in"}>
-                    <img src="/images/css.png" alt="" />
-                </div>
-                <div className="mon" data-aos ={"zoom-in"}>
-                    <img src="/images/js.png" alt="" />
-                </div>
-                <div className="mon" data-aos ={"zoom-in"}>
-                    <img src="/images/git.png" alt="" />
-                </div>
-                <div className="mon" data-aos ={"zoom-in"}>
-                    <img src="/images/vs.png" alt="" />
-                </div>
+                
 
                 <div className="mon" data-aos ={"zoom-in"}>
                     <img src="/images/logo192.png" alt="" />
                 </div>
                
-                <div className="mon"data-aos ={"zoom-in"}>
-                    <img src="/images/redux.png" alt="" />
-                </div>
-
 
                 <div className="mon" data-aos ={"zoom-in"}>
                     <img src="/images/node.png" alt="" />
@@ -51,6 +33,28 @@ function Skill(){
 
                 <div className="mon" data-aos ={"zoom-in"}>
                     <img src="/images/mongo.png" alt="" />
+                </div>
+
+                <div className="mon"data-aos ={"zoom-in"}>
+                    <img src="/images/redux.png" alt="" />
+                </div>
+
+                <div className="mon" data-aos ={"zoom-in"}>
+                    <img src="/images/js.png" alt="" />
+                </div>
+
+                <div className="mon" data-aos ={"zoom-in"}>
+                    <img src="/images/html.png" alt="" />
+                </div>
+                <div className="mon" data-aos ={"zoom-in"}>
+                    <img src="/images/css.png" alt="" />
+                </div>
+               
+                <div className="mon" data-aos ={"zoom-in"}>
+                    <img src="/images/git.png" alt="" />
+                </div>
+                <div className="mon" data-aos ={"zoom-in"}>
+                    <img src="/images/vs.png" alt="" />
                 </div>
             </div>
 
